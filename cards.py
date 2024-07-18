9206e33d0e5b3321a9c738885fc7a338cc6a9f08
GickiAnarchy <mindofmagick85@gmail.com>
2024-07-17 19:05:41 -0500

    afternoon

--- cards.py
+++ cards.py
@@ -154,7 +154,6 @@ class Stack:
     def __init__(self):
         self.cards = []
     
-    
     @property
     def cards(self):
         return self._cards
@@ -163,7 +162,6 @@ class Stack:
         if isinstance(newcards,list):
             self._cards = newcards
         
-
     def addToStack(self, other):
         if isinstance(other, Stack):
             while len(other.cards) > 0:
@@ -243,6 +241,7 @@ class Player:
     def __init__(self, name:str = "Default"):
         self.hand = Hand()
         self.name = name
+        self.phases = []
 
     def showHand(self):
         self.hand.showHand(self.name)
@@ -267,6 +266,76 @@ class Player:
                 
 
 
+###
+###
+goals = [
+    [PhaseSet(3),PhaseSet(3)],
+    [PhaseSet(3), PhaseRun(4)],
+    [PhaseSet(4), PhaseRun(4)],
+    [PhaseRun(7)],
+    [PhaseRun(8)],
+    [PhaseRun(9)],
+    [PhaseSet(4),PhaseSet(4)],
+    [PhaseColor(7)],
+    [PhaseSet(5), PhaseSet(2)],
+    [PhaseSet(5), PhaseSet(3)]
+    ]
+phases_dict = {
+    0:{"name":"Phase 1",
+        "goal":[]
+    },
+    1:{"name":"Phase 2",
+        "goal":[]
+    },
+    2:{"name":"Phase 3",
+        "goal":[]
+    },
+    3:{"name":"Phase 4",
+        "goal":[]
+    },
+    4:{"name":"Phase 5",
+        "goal":[]
+    },
+    5:{"name":"Phase 6",
+        "goal":[]
+    },
+    6:{"name":"Phase 7",
+        "goal":[]
+    },
+    7:{"name":"Phase 8",
+        "goal":[]
+    },
+    8:{"name":"Phase 9",
+        "goal":[]
+    },
+    9:{"name":"Phase 10",
+        "goal":[]
+    },
+    10:{"name":"NA",
+        "goal":[]
+    }
+}
+
+
+class Phase():
+    def __init__(self, number:int,goals:list):
+        self.number = number
+        self.goals = goals
+
+class PhaseGoal(Stack):
+    def __init__(self, ptype:int, min_cards:int):
+        #ptype: 1=Set, 2=Run, 3=Color
+        self.ptype = ptype
+        self.min_cards = min_cards
+        
+    def addToStack(self,other):
+        #check condition
+        super().addToStack(other)
+    
+    def checkSet(self,cards:Stack):
+        cards.sortByNumber()
+        #do check here;
+    
 ###
 ###
 class Game:
--- phases.py
+++ phases.py
@@ -1,59 +1,46 @@
+#THIS FILE WILL BE DELETED!
 
-
-"""
-- 2 sets of 3
-- 1 set of 3 + 1 run of 4
-- 1 set of 4 + 1 run of 4
-- 1 run of 7
-- 1 run of 8
-- 1 run of 9
-- 2 sets of 4
-- 7 cards of one color
-- 1 set of 5 + 1 set of 2
-- 1 set of 5 + 1 set of 3
-
-PhaseSet(3),PhaseSet(3)
-PhaseSet(3), PhaseRun(4)
-PhaseSet(4), PhaseRun(4)
-PhaseRun(7)
-PhaseRun(8)
-PhaseRun(9)
-PhaseSet(4),PhaseSet(4)
-PhaseColor(7)
-PhaseSet(5), PhaseSet(2)
-PhaseSet(5), PhaseSet(3)
-"""
-
-
+goals = [
+    [PhaseSet(3),PhaseSet(3)],
+    [PhaseSet(3), PhaseRun(4)],
+    [PhaseSet(4), PhaseRun(4)],
+    [PhaseRun(7)],
+    [PhaseRun(8)],
+    [PhaseRun(9)],
+    [PhaseSet(4),PhaseSet(4)],
+    [PhaseColor(7)],
+    [PhaseSet(5), PhaseSet(2)],
+    [PhaseSet(5), PhaseSet(3)]
+    ]
 phases_dict = {
-    0:{"name":"NA",
+    0:{"name":"Phase 1",
         "goal":[]
     },
-    1:{"name":"NA",
+    1:{"name":"Phase 2",
         "goal":[]
     },
-    2:{"name":"NA",
+    2:{"name":"Phase 3",
         "goal":[]
     },
-    3:{"name":"NA",
+    3:{"name":"Phase 4",
         "goal":[]
     },
-    4:{"name":"NA",
+    4:{"name":"Phase 5",
         "goal":[]
     },
-    5:{"name":"NA",
+    5:{"name":"Phase 6",
         "goal":[]
     },
-    6:{"name":"NA",
+    6:{"name":"Phase 7",
         "goal":[]
     },
-    7:{"name":"NA",
+    7:{"name":"Phase 8",
         "goal":[]
     },
-    8:{"name":"NA",
+    8:{"name":"Phase 9",
         "goal":[]
     },
-    9:{"name":"NA",
+    9:{"name":"Phase 10",
         "goal":[]
     },
     10:{"name":"NA",
@@ -61,31 +48,21 @@ phases_dict = {
     }
 }
 
+
+class Phase(Stack):
+    def __init__(self, number:int,goals:list):
+        self.number = number
+        self.goals = goals
+
+
+
+
+"""
 class PhaseStack:
     def __init__(self, min_cards:int):
         self.min_cards = min_cards
         self.cards = Stack()
         self.isComplete = False
-    
-    def addCards(self, cards):
-        if isinstance(cards, Stack):
-            self.cards.combineStack(cards)
-
-    def checkCards(self, cards):
-        pass
-
-    def complete(self):
-        self.isComplete = True
-    
-    @property
-    def isComplete(self):
-        return self._isComplete
-    @isComplete.setter
-    def isComplete(self,newone:bool):
-        if self._isComplete == False:
-            self._isComplete = newone
-        elif self._isComplete == True:
-            self._isComplete = True
 
 class PhaseSet(PhaseStack):
     def __init__(self, min_cards:int):
@@ -105,4 +82,7 @@ class PhaseRun(PhaseStack):
 class PhaseColor(PhaseStack):
     def __init__(self, min_cards:int):
         super().__init__(min_cards)
-        
+
+
+
+"""
