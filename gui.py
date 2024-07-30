7d1bcf57b5ca573e1dc55d7dd410dc9f1ae59b30
GickiAnarchy <mindofmagick85@gmail.com>
2024-07-29 20:28:59 -0500

    automated commit

--- gui.py
+++ gui.py
@@ -1,9 +1,28 @@
 import random
 import copy
 import os
-import pygame
 from .cards import *
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 
 
-pygame.init()
 
+class PlayerCreation(GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text='Player Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+
+
+
+class MyApp(App):
+    def build(self):
+        return PlayerCreation()
+
+
+if __name__ == "__main__":
+    MyApp.run()
