9edcce46446f740f81cb5a38cfd362a7f139f2bd
GickiAnarchy <mindofmagick85@gmail.com>
2024-08-06 22:00:32 -0500

    little

--- __init__.py
+++ __init__.py
@@ -3,5 +3,8 @@ from .cards import Stack, Deck, Hand
 from .cards import Phase, Goal, SetGoal, RunGoal, ColorGoal
 from .cards import Player, GameApp
 
+from .gui import Phase10App
 
-__all__ = ["Card", "WildCard", "SkipCard", "BasicCard", "LowCard", "HighCard", "Deck", "Stack", "Hand", "Player", "Phase", "Goal", "SetGoal", "RunGoal", "ColorGoal", "GameApp"]
+
+
+__all__ = ["Card", "WildCard", "SkipCard", "BasicCard", "LowCard", "HighCard", "Deck", "Stack", "Hand", "Player", "Phase", "Goal", "SetGoal", "RunGoal", "ColorGoal", "GameApp", "Phase10App"]
---
+++ main.py
@@ -0,0 +1,5 @@
+from gui import Phase10App
+
+
+if __name__ == "__main__":
+    Phase10App().run()
