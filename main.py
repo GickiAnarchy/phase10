4342d2b5470fd77579b06508c98cfea46145c336
GickiAnarchy <mindofmagick85@gmail.com>
2024-08-07 04:18:24 -0500

    little

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-from gui import Phase10App
+from kivy.app import App
 
+
+class TestApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    TestApp().run()
+
+
+
+"""
+from gui import Phase10App
 
 if __name__ == "__main__":
     Phase10App().run()
+"""
