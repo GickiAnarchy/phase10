9707f7a15c292fb4e60f7657a31eb35d85df910c
FatherAnarchy <70991592+GickiAnarchy@users.noreply.github.com>
2024-08-09 13:02:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-from kivy.app import App
-
-
-class TestApp(App):
-    pass
-
-
-if __name__ == '__main__':
-    TestApp().run()
 
 
 
-"""
 from gui import Phase10App
 
 if __name__ == "__main__":
     Phase10App().run()
-"""
