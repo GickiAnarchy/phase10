db24c7ada0188dc641887753530cf8f7e4fcc077
FatherAnarchy <70991592+GickiAnarchy@users.noreply.github.com>
2024-10-11 20:39:28 -0500

    update phase10/gui.py

--- phase10/gui.py
+++ phase10/gui.py
@@ -83,7 +83,7 @@ cl = GameClient()
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(AsyncApp().app_func())
+    loop.run_until_complete(PhaseTenApp().app_func())
     loop.close()
 
 
