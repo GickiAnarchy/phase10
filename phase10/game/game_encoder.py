a0e3bb0adafa73a7506c2e53739094d77029f3d5
GickiAnarchy <mindofmagick85@gmail.com>
2024-11-28 05:10:44 -0600

    rewrite

--- phase10/game/game_encoder.py
+++ phase10/game/game_encoder.py
@@ -3,7 +3,7 @@ from phase10.game.classes.goal import Goal
 from phase10.game.classes.phase import Phase
 from phase10.game.classes.player import Player
 from phase10.game.classes.card import Card
-
+from phase10.game.games.base import BaseGame, RPS
 
 class GameEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -15,8 +15,8 @@ class GameEncoder(json.JSONEncoder):
             return {'__player__': True, **obj.to_dict()}
         elif isinstance(obj, Card):
             return {'__card__': True, **obj.to_dict()}
-        elif isinstance(obj, War):
-            return {'__war__': True, **obj.to_dict()}
+        elif isinstance(obj, BaseGame):
+            return {'__rps__': True, **obj.to_dict()}
         return super().default(obj)
 
 
@@ -29,4 +29,6 @@ def game_decoder(obj):
         return Player.from_dict(obj)
     elif '__card__' in obj:
         return Card.from_dict(obj)
+    elif '__rps__' in obj:
+        return RPS.from_dict(obj)
     return obj
---
+++ phase10/game/gui/minigames.py
@@ -0,0 +1,9 @@
+
+
+
+
+from phase10.game.games.minigames import RPS
+
+
+class RPSScreen(Screen):
+    pass
